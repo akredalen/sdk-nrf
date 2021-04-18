81a9511b917e7c78f6ce137144ec9290ea5280c1
Stine Åkredalen <stine.akredalen@nordicsemi.no>
2021-04-18 23:21:32 +0200

    Added DFU impl. files to Settings Client.

--- samples/bluetooth/mesh/settings_client/src/main.c
+++ samples/bluetooth/mesh/settings_client/src/main.c
@@ -11,7 +11,35 @@
 #include <bluetooth/mesh/models.h>
 #include <bluetooth/mesh/dk_prov.h>
 #include <dk_buttons_and_leds.h>
+#include <zephyr.h>
+
+#include "pca20036_ethernet.h"
+#include "ethernet_command_system.h"
+#include "ethernet_dfu.h"
+#include "hp_led.h"
 #include "model_handler.h"
+
+/////////////////////////////// ETHERNET RX WORK ///////////////////////////////
+
+static struct k_delayed_work ethernet_rx_work;
+
+static void ethernet_rx_work_handler(struct k_work *work)
+{
+	ethernet_command_system_rx();
+	k_delayed_work_submit(&ethernet_rx_work, K_MSEC(10));
+}
+
+static void ethernet_rx_work_init_start(void)
+{
+	k_delayed_work_init(&ethernet_rx_work, ethernet_rx_work_handler);
+	// Initialize a delayed work item, 
+	// with a handler function to invoke each time work item is processed
+
+	k_delayed_work_submit(&ethernet_rx_work, K_NO_WAIT);
+	// Submits a delayed work item to the system workqueue of this type
+}
+
+/////////////////////////////// MESH INIT ///////////////////////////////
 
 static void bt_ready(int err)
 {
@@ -45,10 +73,33 @@ void main(void)
 {
 	int err;
 
-	printk("Initializing...\n");
+	printk("Initializing Bluetooth...\n");
 
 	err = bt_enable(bt_ready);
 	if (err) {
 		printk("Bluetooth init failed (err %d)\n", err);
 	}
+
+	// printk("- PCA20036 sample -\n");
+	printk("- DFU Version: %d -\n", DFU_APP_VERSION);
+
+	err = hp_led_init();
+
+	if (err) {
+		printk("Error initializing HP LED\n");
+		return;
+	}
+
+	err = pca20036_ethernet_init();
+
+	if (err) {
+		printk("Error initializing buttons\n");
+		return;
+	}
+
+	ethernet_rx_work_init_start();
+
+	printk("- Initiated -\n");
+
+	/* DHCP may not be leased yet - check flag */
 }
